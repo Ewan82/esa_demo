d24912b0a9cac3864cd08aa8639652e7ad4de42c
Ewan Pinnington <ewan.pinnington@gmail.com>
2019-03-18 11:11:44 +0000

    add environment.yml

--- signaturesimulator/backscatter.py
+++
@@ -1,9 +0,0 @@
-
-
-class BackScatter:
-    """Class to hold backscatter data for microwave RT model.
-    """
-    def __init__(self):
-        self.hh = None
-        self.vv = None
-        self.hv = None
