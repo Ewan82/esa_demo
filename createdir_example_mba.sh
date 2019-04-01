8488c626670d74d08859907bc481ccd8074ba83c
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2019-04-01 13:51:31 +0200

    slight revision on mba_intro

--- createdir_example_mba.sh
+++ createdir_example_mba.sh
@@ -3,7 +3,7 @@
 rm -rf mba_intro/example_mba
 cp -r retrieval_tool_examples/ret_code1.7 mba_intro/example_mba
 cp    mba_intro/target_schedule_S??_2017.asc mba_intro/example_mba/input
-(cd   mba_intro/example_mba; rm Make-local.inc;
- ln -fs ../Make-mba.inc Make-local.inc;
+(cd   mba_intro/example_mba;
+ ln -fs ../Make-mba.inc Make-debug.inc;
  ln -fs ../../explist_post.py .)
 (cd   mba_intro/example_mba; make setup; make simulate.x runua.x)
