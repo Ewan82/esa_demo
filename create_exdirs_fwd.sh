6081ea611177ee89c0810c1565ae05b839dfa94f
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2019-04-02 18:41:41 +0200

    added script to precompile executables, revised mba_intro script

---
+++ create_exdirs_fwd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -r tool_intro/example_synthetic
+cp -r retrieval_tool_examples/ret_code1.7 tool_intro/example_synthetic
+( cd tool_intro/example_synthetic; make simulate.x runretrieval.x )
+
+rm -rf mba_intro/example_mba
+cp -r retrieval_tool_examples/ret_code1.7 mba_intro/example_mba
+cp    mba_intro/target_schedule_S??_2017.asc mba_intro/example_mba/input
+(cd   mba_intro/example_mba;
+ ln -fs ../Make-mba.inc Make-debug.inc;
+ ln -fs ../../explist_post.py .)
+(cd   mba_intro/example_mba; make setup; make simulate.x runua.x)
